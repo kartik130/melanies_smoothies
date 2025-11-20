67e92fc3f812e847195f364a9edaf8194367d625
kartik130 <96197129+kartik130@users.noreply.github.com>
2025-11-20 19:32:39 +0530

    Update steamlit_app.py

--- steamlit_app.py
+++ steamlit_app.py
@@ -15,7 +15,7 @@ st.write('The name on your smoothie will be:', name_on_order)
 
 cnx = st.connection("snowflake")
 session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME')
 #st.dataframe(data=my_dataframe, use_container_width=True)
 #st.stop()
 # Convert the Snowpark Dataframe to a Pandas Dataframe so we can use the LOC function
@@ -36,7 +36,7 @@ if ingredients_list:
       search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
       st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
       st.subheader(fruit_chosen + ' Nutrition Information')
-      smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+      smoothiefroot_response = requests.get(f"https://my.smoothiefroot.com/api/fruit/{search_on}")
       sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
     #st.write(ingredients_string)
